c195297e3c8a5fcfd1f196699ad8f43aa111360b
Jose Gutierrez Marin <andres.gutierrez-m@mail.escuelaing.edu.co>
2021-02-10 09:04:09 -0500

    Complete

--- src/main/java/edu/eci/arsw/springdemo/EnglishSpellChecker.java
+++ src/main/java/edu/eci/arsw/springdemo/EnglishSpellChecker.java
@@ -1,11 +1,17 @@
 package edu.eci.arsw.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+//@Service
 public class EnglishSpellChecker implements SpellChecker {
 
 	@Override
 	public String checkSpell(String text) {		
 		return "Checked with english checker:"+text;
 	}
+	
+	
 
         
 }
--- src/main/java/edu/eci/arsw/springdemo/SpanishSpellChecker.java
+++ src/main/java/edu/eci/arsw/springdemo/SpanishSpellChecker.java
@@ -3,6 +3,10 @@ package edu.eci.arsw.springdemo;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SpanishSpellChecker implements SpellChecker {
 
 	@Override
